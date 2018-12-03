acf1bd7b6b4c4c52ae0fa42b9b31cd1de243529e
Will Harris <Harriswill22@yahoo.com>
2018-12-03 10:43:30 -0500

    adds score card

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+  
+  // set up state
+  this.state = {
+    scores: [
+      {
+        id: 1,
+        name: "Will",
+        score: 10
+      },
+      {
+        id: 2,
+        name: "Rick",
+        score: 13
+      },
+      {
+        id: 3,
+        name: "Dan",
+        score: 18
+      }
+    ]
   }
 }
+render() {
+  return ( 
+    <div className = "App">
+    <h1> Score Board App </h1> 
+    <div className = "score-container"> {this._scoreAsCards()}</div>
+    </div>
+  );
+}
+_scoreAsCards(){
+  const cards = this.state.scores.map(score => {
+    return (
+      <div key ={score.id}>
+        <h2>Name {score.name} // Score: {score.score}</h2>
+      </div>
+    )
+  });
+  return cards;
+}
+
+}
+
 
 export default App;
